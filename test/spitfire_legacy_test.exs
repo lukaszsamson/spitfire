8d34cf5aa69da05da8a389330a05d92ec32b8908
Lukasz Samson <lukaszsamson@gmail.com>
2025-09-15 20:54:13 +0200

    wip

--- test/spitfire_legacy_test.exs
+++ test/spitfire_legacy_test.exs
@@ -3030,6 +3030,28 @@ defmodule SpitfireLegacyTest do
     end
   end
 
+  test "elixir sources" do
+    files =
+      Enum.module_info()[:compile][:source]
+      |> Path.join("../../..")
+      |> Path.expand()
+      |> Path.join("**/*.ex*")
+      |> Path.wildcard()
+
+    for file <- files do
+      code = file |> File.read!()
+      # lines = String.split(source, "\n")
+      # assert Spitfire.parse(code) == s2q(code)
+      res = Spitfire.parse(code) == s2q(code)
+
+      if not res do
+        IO.puts(file)
+      end
+
+      # assert res
+    end
+  end
+
   defp s2q(code, opts \\ []) do
     Code.string_to_quoted(
       code,
